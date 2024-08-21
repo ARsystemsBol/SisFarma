7d25909563d8c51957006ae6f04d24f0403a06f6
adriDev <a.r.sistemsas@gmail.com>
2024-08-20 22:19:52 -0400

    vistas listas - revisar los procedimientos

---
+++ BD/tablas0.sql
@@ -0,0 +1,8 @@
+TRUNCATE TABLE cajas;
+TRUNCATE TABLE lote;
+TRUNCATE TABLE detallelote;
+TRUNCATE TABLE detalle_temp_lote;
+TRUNCATE TABLE traspaso;
+TRUNCATE TABLE detalletraspaso;
+TRUNCATE TABLE detalle_temp_traspaso;
+TRUNCATE TABLE devolucion_suc;
